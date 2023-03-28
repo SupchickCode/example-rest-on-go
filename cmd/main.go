93f96b7a22a16843b7a4db5003f06b30a9927cb8
ivan <ivan@192.168.1.104>
2023-03-28 23:39:51 +0300

    edit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/SupchickCode/simpleRestAPI"
+)
+
+func main() {
+	srv := new(simpleRestAPI.Server)
+
+	if err := srv.Run(":8000"); err != nil {
+		log.Fatalf("Error while running http serve : %s", err.Error())
+	}
 }
