4d99a39378afc1849288dc8db59dbe2c7cb58a06
ivan <ivan@192.168.1.104>
2023-03-30 01:16:44 +0300

    edit new str and init serve and first hanlers

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,14 @@ import (
 	"log"
 
 	"github.com/SupchickCode/simpleRestAPI"
+	"github.com/SupchickCode/simpleRestAPI/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(simpleRestAPI.Server)
 
-	if err := srv.Run(":8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error while running http serve : %s", err.Error())
 	}
 }
