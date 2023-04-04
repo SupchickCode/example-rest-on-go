b59b19b6497ac952952c92e20061a7f30f0b0136
ivan <ivan@192.168.1.104>
2023-04-05 01:23:34 +0300

    edit DI

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,15 @@ import (
 
 	"github.com/SupchickCode/simpleRestAPI"
 	"github.com/SupchickCode/simpleRestAPI/pkg/handler"
+	"github.com/SupchickCode/simpleRestAPI/pkg/repository"
+	"github.com/SupchickCode/simpleRestAPI/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repo := repository.NewRepository()
+	services := service.NewService(repo)
+	handlers := handler.NewHandler(services)
+
 	srv := new(simpleRestAPI.Server)
 
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
